b1b012fab7d24beae94123ed132c1f4602b1db8b
sakurm <sudhirkumar.kurma@planonsoftware.com>
2025-11-01 16:12:42 +0530

    Commiting partial changes

--- expensetracker/src/main/java/com/expensetracker/config/CorsConfig.java
+++ expensetracker/src/main/java/com/expensetracker/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:5173", "http://localhost:3000")
+                        .allowedOrigins("https://expensetrackerreactui.vercel.app/", "http://localhost:3000")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
