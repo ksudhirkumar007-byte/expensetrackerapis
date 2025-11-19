995e0a56a5dc5f0bb538582b7529c57c9c6e2019
sakurm <ksudhirkumar007@gmail.com>
2025-11-19 17:12:06 +0530

    cross origin url changed to render

--- expensetracker/src/main/java/com/expensetracker/config/CorsConfig.java
+++ expensetracker/src/main/java/com/expensetracker/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("https://expensetrackerreactui.vercel.app/", "http://localhost:3000")
+                        .allowedOrigins("https://expensetrackerupdatedui.onrender.com/", "http://localhost:3000")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
