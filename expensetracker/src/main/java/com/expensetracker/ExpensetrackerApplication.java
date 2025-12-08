e4885f81b6043dfdfd65cd8a59626d7bf7e65a95
sakurm <ksudhirkumar007@gmail.com>
2025-12-08 21:44:31 +0530

    import added

--- expensetracker/src/main/java/com/expensetracker/ExpensetrackerApplication.java
+++ expensetracker/src/main/java/com/expensetracker/ExpensetrackerApplication.java
@@ -1,5 +1,6 @@
 package com.expensetracker;
 
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
