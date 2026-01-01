9d7a97eede868a9bc367dabf4122dba97b7a6169
sakurm <ksudhirkumar007@gmail.com>
2026-01-01 22:22:34 +0530

    feign client interceptor changes

--- expensetracker/src/main/java/com/expensetracker/config/FeignJwtInterceptor.java
+++ expensetracker/src/main/java/com/expensetracker/config/FeignJwtInterceptor.java
@@ -2,9 +2,13 @@ package com.expensetracker.config;
 
 import feign.RequestInterceptor;
 import feign.RequestTemplate;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 @Component
 public class FeignJwtInterceptor implements RequestInterceptor {
